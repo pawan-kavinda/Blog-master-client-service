692ddcdaec1dc5e22c7170f15931188e4d4affc1
pawan-kavinda <pawankavinda09@gmail.com>
2025-08-22 23:44:05 +0530

    url

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
-export const BLOG_SERVICE_URL = 'http://localhost:4000/api/posts'
+export const BLOG_SERVICE_URL = 'http://localhost:4000/api'
 export const AUTH_SERVICE_URL = 'http://54.197.222.190:4001/auth'
 
+// export const BLOG_SERVICE_URL = 'http://localhost:4000/api'
+// export const AUTH_SERVICE_URL = 'http://localhost:4001/auth'
